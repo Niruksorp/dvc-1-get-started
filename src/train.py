4119a1658848e76b89d577ac1eb07cb43ed30b22
Проскурин Глеб Егорович 4057678 <proskuring@vtb.ru>
2022-11-08 01:15:58 +0700

    decision-tree

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 import np as np
 import pandas as pd
 import numpy as np
-from sklearn.cluster import KMeans
+from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     X_train = pd.read_csv('data/prepared/X_train.csv')
     y_train = pd.read_csv('data/prepared/y_train.csv')
 
-    classifier = KMeans()
+    classifier = DecisionTreeClassifier()
     # Applying classifier on training data
     classifier = classifier.fit(X_train, y_train)
     joblib.dump(classifier, 'model/model.joblib')
