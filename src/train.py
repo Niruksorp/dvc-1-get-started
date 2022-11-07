003bed4a3b9519b4b46f92dea63f1852bfbc3b24
Проскурин Глеб Егорович 4057678 <proskuring@vtb.ru>
2022-11-08 01:07:17 +0700

    k-means-pipeline

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
 import np as np
 import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.cluster import KMeans
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 
@@ -11,11 +11,7 @@ if __name__ == '__main__':
     X_train = pd.read_csv('data/prepared/X_train.csv')
     y_train = pd.read_csv('data/prepared/y_train.csv')
 
-    # create classifier
-    from sklearn.tree import DecisionTreeClassifier
-    from sklearn import tree
-
-    classifier = DecisionTreeClassifier(criterion="entropy")
+    classifier = KMeans()
     # Applying classifier on training data
     classifier = classifier.fit(X_train, y_train)
     joblib.dump(classifier, 'model/model.joblib')
